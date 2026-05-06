e53d9bba83d0c74dfd52c152b6eec2dd17695d81
Isabellagg19 <isabellaguioc123@gmail.com>
2026-05-06 05:23:46 -0500

    new quiz question

--- eng.js
+++ eng.js
@@ -1,10 +1,19 @@
-function checkAnswer(answer) {
-    const result = document.getElementById("result1");
-    const correctanswer = "true";
-    if (answer === correctanswer) {
-        result.textContent = "You are right yeyy, it was in tokio, nice work"
+function checkAnswer1(answer) {
+    const result1 = document.getElementById("result1");
+    const correctanswer1 = "true";
+    if (answer === correctanswer1) {
+        result1.textContent = "You are right yeyy, it was in tokio, nice work"
     } else {
-        result.textContent ="Nice try!, but no, it was actually in Tokio, keep reading to find out how"
+        result1.textContent ="Nice try!, but no, it was actually in Tokio, keep reading to find out how"
     }
+}
 
+function checkAnswer2(answer) {
+    const result2 = document.getElementById("result2");
+    const correctanswer2 = "right";
+    if (answer === correctanswer2) {
+        result2.textContent = "That's... correct, good job, keep reading to find out how this is possible!!"
+    } else {
+        result2.textContent ="That's... Incorrect, I'm sorry, but keep reading if you want to keep learning the amazing world of fungi"
+    }
 }
