576b92e1de4ddf5ea301732a1e10230b6d3f28ef
Isabellagg19 <isabellaguioc123@gmail.com>
2026-05-07 14:21:19 -0500

    new button and container in medicine page

---
+++ med.js
@@ -0,0 +1,9 @@
+function toggleContainer() {
+    const container = document.getElementById("hospital-container");
+    
+    if (container.style.display === "none" || container.style.display === "") {
+        container.style.display = "block";
+    } else {
+        container.style.display = "none";
+    }
+}
